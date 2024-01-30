abb1f096e324df7e79a04803eb693bd0572d3675
Seth Kunzler <sethkunzler@gmail.com>
2024-01-29 17:00:37 -0700

    negative points and a check bellow 0

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ let victoryBannerElement = document.getElementById('victoryBanner')
 
 function drawHomeScore(score) {
   scoreHome += score
+  checkBellowZero()
   homeScoreElement.innerText = scoreHome 
   endGame()
 }
 function drawAwayScore(score) {
   scoreAway += score
+  checkBellowZero()
   awayScoreElement.innerText = scoreAway
   endGame()
 }
@@ -24,13 +26,21 @@ function reset() {
   homeScoreElement.innerText = scoreHome
   awayScoreElement.innerText = scoreAway
 }
+function checkBellowZero() {
+  if (scoreHome <= 0) {
+    scoreHome = 0 
+  }
+  else if (scoreAway <= 0) {
+    scoreAway = 0 
+  }
+}
 function endGame() {
-  if (scoreAway == 21) {
+  if (scoreAway >= 21) {
     console.log("Away wins!")
     reset()
     victoryBannerElement.innerHTML = `<h2>Away Wins!</h2><iframe width="560" height="315" src="https://www.youtube.com/embed/Rjap4R1b6p8?si=bhpP6SaEk2ddrALq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
   }
-  else if (scoreHome == 21) {
+  else if (scoreHome >= 21) {
     console.log("Home wins!")
     reset()
     victoryBannerElement.innerHTML = `<h2>Home Wins!</h2><iframe width="560" height="315" src="https://www.youtube.com/embed/04854XqcfCY?si=qXDHC4McPnINsHn1&amp;start=30" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
